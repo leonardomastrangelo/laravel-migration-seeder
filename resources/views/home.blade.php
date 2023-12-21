92dd09f9fcf0cce432cb50275ce86a5e85406aae
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-12-21 15:49:04 +0100

    showed results in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,62 @@
 
 @section('content')
 <main>
-    <h1>Home page</h1>
+    <h1 class="text-center display-1 fw-bold py-5">Trenitalia</h1>
 
-    <ul>
-        @foreach ($trains as $train)
-        <li>
+<div class="container py-5">
+    <table class="table table-primary">
+      <thead>
+        <tr>
+          <th scope="col">agency</th>
+          <th scope="col">departure station</th>
+          <th scope="col">arrival station</th>
+          <th scope="col">departure time</th>
+          <th scope="col">arrival time</th>
+          <th scope="col">code</th>
+          <th scope="col">wagons</th>
+          <th scope="col">on time</th>
+          <th scope="col">canceled</th>
+        </tr>
+      </thead>
+      <tbody>
+        @forelse ($trains as $train)
+        <tr>
+          <td>
             {{$train->agency}}
-        </li>
-        @endforeach
-    </ul>
+          </td>
+          <td>
+            {{$train->departure_station}}
+          </td>
+          <td>
+            {{$train->arrival_station}}
+          </td>
+          <td>
+            {{$train->departure_time}}
+          </td>
+          <td>
+            {{$train->arrival_time}}
+          </td>
+          <td>
+            {{$train->code}}
+          </td>
+          <td>
+            {{$train->wagons}}
+          </td>
+          <td>
+            {{($train->on_time) ? 'timely' : 'delay'}}
+          </td>
+          <td>
+            {{($train->canceled ? 'set' : 'canceled')}}
+          </td>
+          
+        </tr>
+        @empty
+            <h2>No records</h2>
+        @endforelse
+        
+      </tbody>
+    </table>
+</div>
 </main>
 
 @endsection
