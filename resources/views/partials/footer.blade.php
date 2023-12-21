7e81157d37a51e3ff1475c7c07f862eebc0e0948
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-12-21 15:26:57 +0100

    added footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,15 @@
-<div>footer</div>
+<footer>
+    <div class="container">
+        <div class="card text-bg-dark">
+            <div class="card-header">
+                Quote of the day
+            </div>
+            <div class="card-body">
+                <blockquote class="blockquote mb-0">
+                    <p>"The most important thing is to try and inspire people so that they can be great in whatever they want to do."</p>
+                    <div class="blockquote-footer">Kobe Bryant</div>
+                </blockquote>
+            </div>
+        </div>
+    </div>
+</footer>
